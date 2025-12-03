b94778fc5a1fc7eff4c98b6c0afaeccb82f518b1
李承轩 <1206817338@qq.com>
2025-12-03 16:10:01 +0800

    滑动窗口代码

---
+++ huadong_windows.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        n = len(nums)
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+
+        ans= [-q[0][0]]
+
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i-k:
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+        return ans
